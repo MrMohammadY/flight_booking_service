8b5175e915491469161136d4c9dab4bf0fc536c5
MrMohammadY <m.yazdani000@gmail.com>
2022-01-24 14:48:53 +0330

    Update(accounts/models): update custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,18 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.utils.translation import gettext_lazy as _
 from django.db import models
+
+from accounts.validators import phone_number_validation
 
 
 class User(AbstractUser):
     """
     AbstractUser has some problems like username not case-sensitive or email not unique,
      we fix problem here and add some custom fields.
-    you can read about AbstractUser problems here:
-    https://simpleisbetterthancomplex.com/article/2021/07/08/what-you-should-know-about-the-django-user-model.html
-
+    you can read about AbstractUser problems here: https://simpleisbetterthancomplex.com/article/2021/07/08/what-you-should-know-about-the-django-user-model.html
     note: we can be case-sensitive username, but we should use postgresql as database
     """
-    email = models.EmailField(_('email address'), unique=True)  # we set unique true
+    email = models.EmailField(_('email address'), unique=True)
     phone_number = models.CharField(
         _('phone number'),
         max_length=12,
@@ -22,10 +21,8 @@ class User(AbstractUser):
         error_messages={
             'unique': _("A user with that phone number already exists."),
         },
+        validators=[phone_number_validation]
     )
-
-    otp = models.PositiveSmallIntegerField(_('otp'))
-    otp_expire_time = models.DateTimeField(_('otp expire time'), default=None, null=True)
 
     class Meta:
         verbose_name = _('user')
