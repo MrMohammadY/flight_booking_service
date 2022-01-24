d667006e99bb5aa9af70716c62f28324ece18fd5
MrMohammadY <m.yazdani000@gmail.com>
2022-01-24 19:13:25 +0330

    Create(basket): create basket models and admin

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user__username', 'is_paid')
+    list_filter = ('is_paid',)
+    search_fields = ('user__username',)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        'flight__flight_number', 'flight__from_city', 'flight__to_city',
+        'cart', 'first_name', 'last_name', 'national_code', 'birthday'
+    )
+    list_filter = ('flight__from_city', 'flight__to_city')
