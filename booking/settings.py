23d691dd7e79c96d19c788748c8835b0be9c6fd6
MrMohammadY <m.yazdani000@gmail.com>
2022-01-24 14:53:40 +0330

    Update(settings): add custom authentication in settings

--- booking/settings.py
+++ booking/settings.py
@@ -123,6 +123,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.authentication.HashedPasswordAuthBackend',
+]
+
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
