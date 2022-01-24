15c8bbc60806dcf6b68d93a39cfe72256ebc0868
MrMohammadY <m.yazdani000@gmail.com>
2022-01-24 18:24:03 +0330

    Create(flight): create flight models and admins

--- flight/admin.py
+++ flight/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(AirLine)
+class AdminAirLine(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Plane)
+class AdminPlane(admin.ModelAdmin):
+    list_display = ('name', 'airline', 'capacity')
+    list_filter = ('airline',)
+    search_fields = ('name', 'airline__name')
+
+
+@admin.register(Flight)
+class AdminFlight(admin.ModelAdmin):
+    list_display = ('plane', 'from_city', 'to_city', 'depart_datetime', 'arrive_datetime')
+    list_filter = ('from_city', 'to_city')
+    search_fields = ('plane', 'from_city__name', 'to_city__name')
+
+
+@admin.register(Seat)
+class AdminSeat(admin.ModelAdmin):
+    list_display = ('number', 'is_reserve')
+    list_filter = ('is_reserve',)
+
+
+@admin.register(FlightSeat)
+class AdminFlightSeat(admin.ModelAdmin):
+    list_display = ('flight', 'seat', 'customer')
+
+
+@admin.register(State)
+class AdminState(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(City)
+class AdminCity(admin.ModelAdmin):
+    list_display = ('name', 'state')
+    search_fields = ('name',)
--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,118 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from libs.models import BaseModel
+
+User = get_user_model()
+
+
+class State(BaseModel):
+    name = models.CharField(verbose_name=_('name'), max_length=80)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('State')
+        verbose_name_plural = _('States')
+        db_table = 'state'
+
+
+class City(BaseModel):
+    name = models.CharField(verbose_name=_('name'), max_length=80)
+    state = models.ForeignKey(State, related_name='cities', on_delete=models.CASCADE, verbose_name=_('state'))
+
+    def __str__(self):
+        return f'{self.name} - {self.state}'
+
+    class Meta:
+        verbose_name = _('City')
+        verbose_name_plural = _('Cities')
+        db_table = 'city'
+
+
+class AirLine(BaseModel):
+    name = models.CharField(verbose_name=_('name'), max_length=80)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Air Line')
+        verbose_name_plural = _('Air Lines')
+        db_table = 'airline'
+
+
+class Plane(BaseModel):
+    name = models.CharField(verbose_name=_('name'), max_length=50)
+    airline = models.ForeignKey(AirLine, related_name='planes', on_delete=models.CASCADE, verbose_name=_('air line'))
+    capacity = models.PositiveSmallIntegerField(verbose_name=_('capacity'))
+
+    def __str__(self):
+        return f'{self.name} - {self.airline} - {self.capacity}'
+
+    class Meta:
+        verbose_name = _('Plane')
+        verbose_name_plural = _('Planes')
+        db_table = 'plane'
+
+
+class Flight(BaseModel):
+    plane = models.ForeignKey(Plane, related_name='flights', on_delete=models.CASCADE, verbose_name=_('plane'))
+    from_city = models.ForeignKey(
+        City,
+        related_name='origin_flights',
+        on_delete=models.CASCADE,
+        verbose_name=_('origin')
+    )
+    to_city = models.ForeignKey(
+        City,
+        related_name='destination_flights',
+        on_delete=models.CASCADE,
+        verbose_name=_('destination')
+    )
+    depart_datetime = models.DateTimeField(verbose_name=_('depart datetime'))
+    arrive_datetime = models.DateTimeField(verbose_name=_('modified time'))
+
+    def prototype_create_seats(self):
+        for c in self.plane.capacity:
+            FlightSeat.objects.create(flight=self, seat=Seat.objects.create(number=c))
+
+    def __str__(self):
+        return f'{self.plane} - {self.from_city} - {self.to_city}'
+
+    class Meta:
+        verbose_name = _('Flight')
+        verbose_name_plural = _('Flights')
+        db_table = 'flight'
+
+
+class Seat(BaseModel):
+    number = models.PositiveSmallIntegerField(verbose_name=_('number'))
+    is_reserve = models.BooleanField(verbose_name=_('is reserve'), default=False)
+
+    def __str__(self):
+        return f''
+
+    class Meta:
+        verbose_name = _('Seat')
+        verbose_name_plural = _('Seats')
+        db_table = 'seat'
+
+
+class FlightSeat(BaseModel):
+    flight = models.ForeignKey(Flight, related_name='flight_seats', on_delete=models.CASCADE, verbose_name=_('flight'))
+    seat = models.ForeignKey(Seat, related_name='flight_seats', on_delete=models.CASCADE, verbose_name=_('seat'))
+    customer = models.ForeignKey(
+        User,
+        related_name='flight_seats',
+        on_delete=models.CASCADE,
+        verbose_name=_('customer'),
+        null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = _('Flight Seat')
+        verbose_name_plural = _('Flight Seats')
+        db_table = 'flight_seat'
