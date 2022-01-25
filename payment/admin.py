73038839c0d3e6aef7d7c31de1b6ce84857fd251
MrMohammadY <m.yazdani000@gmail.com>
2022-01-25 14:57:15 +0330

    Create(payment): create model and admin payment

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import Invoice, Payment
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cart', 'price', 'is_paid')
+    list_filter = ('is_paid',)
+    search_fields = ('user__username', 'price')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'user', 'invoice', 'price', 'is_paid')
+    list_filter = ('is_paid',)
+    search_fields = ('user__username', 'price')
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,40 @@
+import uuid as uuid
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from basket.models import Cart
+from libs.models import BaseModel
+
+User = get_user_model()
+
+
+class Invoice(BaseModel):
+    user = models.ForeignKey(User, related_name='invoices', on_delete=models.PROTECT, verbose_name=_('user'))
+    cart = models.ForeignKey(Cart, related_name='invoices', on_delete=models.PROTECT, verbose_name=_('cart'))
+    price = models.IntegerField(verbose_name=_('price'))
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.is_paid}'
+
+    class Meta:
+        verbose_name = _('Invoice')
+        verbose_name_plural = _('Invoices')
+        db_table = 'invoice'
+
+
+class Payment(BaseModel):
+    uuid = models.CharField(max_length=32, default=uuid.uuid4().hex, unique=True, verbose_name=_('uuid'))
+    user = models.ForeignKey(User, related_name='payments', on_delete=models.PROTECT, verbose_name=_('user'))
+    invoice = models.ForeignKey(Invoice, related_name='payments', on_delete=models.PROTECT, verbose_name=_('invoice'))
+    price = models.IntegerField(verbose_name=_('price'))
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.uuid} - {self.is_paid} - {self.price}'
+
+    class Meta:
+        verbose_name = _('Payment')
+        verbose_name_plural = _('Payments')
+        db_table = 'payment'
