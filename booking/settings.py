4ed08458cfd994b17f733aee907a60200e0ca14d
MrMohammadY <m.yazdani000@gmail.com>
2022-01-27 14:32:06 +0330

    Create: add  swagger to project

--- booking/settings.py
+++ booking/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'drf_yasg',
+
     'rest_framework',
     'django_filters',
 
@@ -139,4 +141,19 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+# swagger
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+}
+
+REDOC_SETTINGS = {
+    'LAZY_RENDERING': False,
 }
--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FT Booking",
+        default_version='v1',
+        description="FT Booking(Flight Ticket Booking) is a platform for flight ticket booking.",
+        contact=openapi.Contact(email="mehdy.work@gmail.com"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +22,7 @@ urlpatterns = [
     path('api/v1/flight/', include('flight.api.urls', namespace='flight')),
     path('api/v1/basket/', include('basket.api.urls', namespace='basket')),
     path('api/v1/payment/', include('payment.api.urls', namespace='payment')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
