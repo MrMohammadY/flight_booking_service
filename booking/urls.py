dcd9c93aaccdaf7a97c17e499d6e4a12de64cc9e
MrMohammadY <m.yazdani000@gmail.com>
2022-01-27 12:43:43 +0330

    Update(urls): update and add base urls

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.api.urls', namespace='accounts')),
-    path('api/v1/flight/', include('flight.api.urls', namespace='flight'))
+    path('api/v1/flight/', include('flight.api.urls', namespace='flight')),
+    path('api/v1/basket/', include('basket.api.urls', namespace='basket')),
+    path('api/v1/payment/', include('payment.api.urls', namespace='payment')),
 ]
